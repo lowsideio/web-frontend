1f315c5fea19f018c46fedbc245f5dbcd1e2ef11
Aksels Ledins <aksels.ledins@gmail.com>
2017-11-04 15:34:59 +0100

    (router) add /motorcycles/:model

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+
 import MainSearch from '@/components/MainSearch';
+import MotorcycleDetail from '@/pages/MotorcycleDetail';
 
 Vue.use(Router);
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'MainSearch',
       component: MainSearch,
+    },
+    {
+      path: '/motorcycles/:slug',
+      name: 'MotorcycleDetail',
+      component: MotorcycleDetail,
     },
   ],
 });
