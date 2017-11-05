d257d2caad092648ae3f3fd7a0bcbb74b8f24fcf
Aksels Ledins <aksels.ledins@gmail.com>
2017-11-05 01:35:07 +0100

    (app.vue) reworked header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="app">
-    <img alt="lowside-logo" src="./assets/lowside-logo-blank.svg">
-    <router-link to="/">
-      <h1>lowside.io</h1>
-    </router-link>
+    <header>
+      <router-link to="/">
+        <div class="logo" />
+        <div class="title-description">
+          <div class="title"><h1>lowside.io</h1></div>
+          <div class="description">MOTORCYCLES SEARCH</div>
+        </div>
+      </router-link>
+    </header>
     <router-view />
   </div>
 </template>
@@ -34,12 +39,44 @@ a { color: inherit; }
     text-decoration: inherit;
     color: inherit;
   }
-  h1 {
-    color: #20a4f3;
-  }
   img {
     height: 128px;
     width: 128px;
+  }
+
+  h1 {
+    margin: 0;
+  }
+
+  header {
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+
+  .logo {
+    height: 7rem;
+    width: 7rem;
+    background-image: url(/static/lowside-logo.svg);
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
+  }
+
+  .logo, .title-description {
+    display: inline-block;
+    vertical-align: middle;
+    padding-left: 1rem;
+    text-align: left;
+  }
+
+  .title {
+    color: #ff3366;
+    font-family: 'Quicksand', sans-serif;
+    font-size: 1.5rem;
+  }
+  .description {
+    font-family: 'Permanent Marker', cursive;
+    color: #20a4f3;
   }
 
   @media screen and (max-device-width: 414px) and (max-device-height: 776px) {
